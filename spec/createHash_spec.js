a25693495135c20f8989b7d9395fac748258859d
James Dunkerley <jdunkerley@scottlogic.com>
2015-09-18 15:32:30 +0100

    Got JSCS up and running and tidied up to airbnb config

--- spec/createHash_spec.js
+++ spec/createHash_spec.js
@@ -1,21 +1,22 @@
-describe("createHash", function() {
-	'use strict';
-	var bitCoinHash = require('../bitCoinHash.js');
+describe('createHash', function() {
+  'use strict';
+  var bitCoinHash = require('../bitCoinHash.js');
 
-	it("is a function", function() {
-		var typeName = typeof(bitCoinHash.createHash);
-		expect(typeName).toEqual('function');
-	});
+  it('is a function', function() {
+    var typeName = typeof bitCoinHash.createHash;
+    expect(typeName).toEqual('function');
+  });
 
-	it("should be able to calculate SHA256 for Hello World", function() {
-		var initial = '01000000' +
-		    '81cd02ab7e569e8bcd9317e2fe99f2de44d49ab2b8851ba4a308000000000000' +
-		    'e320b6c2fffc8d750423db8b1eb942ae710e951ed797f7affc8892b0f1fc122b' +
-		    'c7f5d74d' +
-		    'f2b9441a' +
-		    '42a14695';
-		var test = bitCoinHash.createHash(initial);
-		var expected = '00000000000000001e8d6829a8a21adc5d38d0a473b144b6765798e61f98bd1d';
-		expect(test).toEqual(expected);
-	});
+  it('should be able to calculate SHA256 for Hello World', function() {
+    var initial =
+        '01000000' + // Version
+        '81cd02ab7e569e8bcd9317e2fe99f2de44d49ab2b8851ba4a308000000000000' + // previousHash
+        'e320b6c2fffc8d750423db8b1eb942ae710e951ed797f7affc8892b0f1fc122b' + // merkleRoot
+        'c7f5d74d' + // Time
+        'f2b9441a' + // Bits
+        '42a14695'; // Nonce
+    var test = bitCoinHash.createHash(initial);
+    var expected = '00000000000000001e8d6829a8a21adc5d38d0a473b144b6765798e61f98bd1d';
+    expect(test).toEqual(expected);
+  });
 });
--- spec/getBlockFromWeb_spec.js
+++ spec/getBlockFromWeb_spec.js
@@ -1,24 +1,24 @@
-describe("getBlockFromWeb", function() {
-	'use strict';
-	var bitCoinHash = require('../bitCoinHash.js');
-	
-	var complete = false;
-	var result;
+describe('getBlockFromWeb', function() {
+  'use strict';
+  var bitCoinHash = require('../bitCoinHash.js');
 
-	beforeEach(function(done) {
-		bitCoinHash.getBlockFromWeb('https://blockexplorer.com/api/block/000000000019d6689c085ae165831e934ff763ae46a2a6c172b3f1b60a8ce26f')
-			.then(function(block) {
-				result = block;
-				done();
-			});
-	});
+  var complete = false;
+  var result;
 
-	it("is a function", function() {
-		var typeName = typeof(bitCoinHash.getBlockFromWeb);
-		expect(typeName).toEqual('function');
-	});
+  beforeEach(function(done) {
+    bitCoinHash.getBlockFromWeb('https://blockexplorer.com/api/block/000000000019d6689c085ae165831e934ff763ae46a2a6c172b3f1b60a8ce26f')
+      .then(function(block) {
+        result = block;
+        done();
+      });
+  });
 
-	it("should be able to fetch the genesis block", function() {
-		expect(result.hash).toEqual('000000000019d6689c085ae165831e934ff763ae46a2a6c172b3f1b60a8ce26f');
-	});
+  it('is a function', function() {
+    var typeName = typeof bitCoinHash.getBlockFromWeb;
+    expect(typeName).toEqual('function');
+  });
+
+  it('should be able to fetch the genesis block', function() {
+    expect(result.hash).toEqual('000000000019d6689c085ae165831e934ff763ae46a2a6c172b3f1b60a8ce26f');
+  });
 });
