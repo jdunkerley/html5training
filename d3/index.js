bce824f7a4bda6e3ddf60590c333ab208666e242
James Dunkerley <jdunkerley@scottlogic.com>
2015-09-28 20:53:38 +0100

    d3 stuff...

--- d3/index.js
+++ d3/index.js
@@ -2,41 +2,55 @@
   'use strict';
 
   var svg = d3.select('svg');
-  var width = svg.attr('width');
-  var height = svg.attr('height');
+  var margin = {top: 20, right: 30, bottom: 30, left: 40};
+  var width = svg.attr('width') - margin.left - margin.right;
+  var height = svg.attr('height') - margin.top - margin.bottom;
+
+  var x = d3.scale.ordinal()
+    .rangeRoundBands([0, width], 0.01);
+
+  var xAxis = d3.svg.axis()
+    .scale(x)
+    .orient('bottom');
 
   // Create Linear Scale
-  var x = d3.scale.linear()
+  var y = d3.scale.linear()
     .range([0, height]);
 
   d3.tsv('data.tsv', type, function (error, data) {
-    x.domain([0, d3.max(data, function (d) {
+    x.domain(data.map(function(d) { return d.name; }));
+
+    svg.append('g')
+      .attr('class', 'x axis')
+      .attr('transform', 'translate(' + margin.left + ',' + (svg.attr('height') - margin.bottom) + ')')
+      .call(xAxis);
+
+    y.domain([0, d3.max(data, function (d) {
       return d.value;
     })]);
 
-    var barWidth = width / data.length;
-
     // Div Based Chart
-    var bars = svg.selectAll('g')
+    var bars = svg.append('g').attr('transform', 'translate(' + margin.left + ',' + margin.top + ')')
+      .selectAll('g')
       .data(data)
       .enter().append('g')
-      .attr('transform', function (d, i) {
-        return 'translate(' + i * barWidth + ', 0)';
+      .attr('transform', function (d) {
+        return 'translate(' + x(d.name) + ', 0)';
       });
 
     bars.append('rect')
       .attr({
-        y: function (d) { return height - x(d.value); },
+        y: function (d) { return height - y(d.value); },
         height: function (d) {
-          return x(d.value);
+          return y(d.value);
         },
-        width: barWidth - 1
+        width: x.rangeBand()
       });
 
     bars.append('text')
       .attr({
-        x: barWidth / 2,
-        y: function (d) { return height - x(d.value); },
+        x: x.rangeBand() / 2,
+        y: function (d) { return height - y(d.value); },
         dy: '12px'
       })
       .text(function (d) {
