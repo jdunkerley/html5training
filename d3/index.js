3ba79470a3e9c79fe7f9bf8b8b303bed6e42ee96
James Dunkerley <jdunkerley@scottlogic.com>
2015-09-28 16:13:41 +0100

    Rotating to a column based chart

--- d3/index.js
+++ d3/index.js
@@ -1,48 +1,43 @@
 (function() {
   'use strict';
 
-  var body = d3.select('body');
-
-  var width = 420;
-  var barHeight = 20;
-
-  var svg = body.append('svg')
-    .classed('chart', true)
-    .attr('width', width);
+  var svg = d3.select('svg');
+  var width = svg.attr('width');
+  var height = svg.attr('height');
 
   // Create Linear Scale
   var x = d3.scale.linear()
-    .range([0, width]);
+    .range([0, height]);
 
   d3.tsv('data.tsv', type, function (error, data) {
-    svg.attr('height',  data.length * barHeight)
     x.domain([0, d3.max(data, function (d) {
       return d.value;
     })]);
+
+    var barWidth = width / data.length;
 
     // Div Based Chart
     var bars = svg.selectAll('g')
       .data(data)
       .enter().append('g')
       .attr('transform', function (d, i) {
-        return 'translate(0,' + i * barHeight + ')';
+        return 'translate(' + i * barWidth + ', 0)';
       });
 
     bars.append('rect')
       .attr({
-        width: function (d) {
+        y: function (d) { return height - x(d.value); },
+        height: function (d) {
           return x(d.value);
         },
-        height: barHeight - 1
+        width: barWidth - 1
       });
 
     bars.append('text')
       .attr({
-        x: function (d) {
-          return x(d.value) - 3;
-        },
-        y: barHeight / 2,
-        dy: '3px'
+        x: barWidth / 2,
+        y: function (d) { return height - x(d.value); },
+        dy: '12px'
       })
       .text(function (d) {
         return d.name;
