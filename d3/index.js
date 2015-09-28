e7f29ec01ee2b6efb4a96d66bbf4b56c3dff0e38
James Dunkerley <jdunkerley@scottlogic.com>
2015-09-28 16:04:54 +0100

    Reading the data from a TSV

--- d3/index.js
+++ d3/index.js
@@ -2,46 +2,57 @@
   'use strict';
 
   var body = d3.select('body');
-  var data = [4, 8, 15, 16, 23, 42];
 
   var width = 420;
   var barHeight = 20;
 
   var svg = body.append('svg')
     .classed('chart', true)
-    .attr({
-      width: width,
-      height: data.length * barHeight
-    });
-
+    .attr('width', width);
 
   // Create Linear Scale
   var x = d3.scale.linear()
-    .domain([0, d3.max(data)])
     .range([0, width]);
 
-  // Div Based Chart
-  var bars = svg.selectAll('g')
-    .data(data)
-    .enter().append('g')
-    .attr({
-      transform: function (d, i) {
+  d3.tsv('data.tsv', type, function (error, data) {
+    svg.attr('height',  data.length * barHeight)
+    x.domain([0, d3.max(data, function (d) {
+      return d.value;
+    })]);
+
+    // Div Based Chart
+    var bars = svg.selectAll('g')
+      .data(data)
+      .enter().append('g')
+      .attr('transform', function (d, i) {
         return 'translate(0,' + i * barHeight + ')';
-      }});
+      });
 
-  bars.append('rect')
-    .attr({
-      width: x,
-      height: barHeight - 1
-    });
+    bars.append('rect')
+      .attr({
+        width: function (d) {
+          return x(d.value);
+        },
+        height: barHeight - 1
+      });
 
-  bars.append('text')
-    .attr({
-      x: function(d) { return x(d) - 3; },
-      y: barHeight / 2,
-      dy: '3px'
-    })
-    .text(function(d) { return d; });
+    bars.append('text')
+      .attr({
+        x: function (d) {
+          return x(d.value) - 3;
+        },
+        y: barHeight / 2,
+        dy: '3px'
+      })
+      .text(function (d) {
+        return d.name;
+      });
 
-  bars.exit().remove();
+    bars.exit().remove();
+  });
+
+  function type(d) {
+    d.value = +d.value; // coerce to number
+    return d;
+  }
 })();
