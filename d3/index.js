0a6038d2ecf2b5307da2bb2edfd1c8806b579fa2
James Dunkerley <jdunkerley@scottlogic.com>
2015-09-29 09:40:20 +0100

    Adding axis and labels

--- d3/index.js
+++ d3/index.js
@@ -3,11 +3,13 @@
 
   var svg = d3.select('svg');
   var margin = {top: 20, right: 30, bottom: 30, left: 40};
+
+  var chart = svg.append('g').attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
   var width = svg.attr('width') - margin.left - margin.right;
   var height = svg.attr('height') - margin.top - margin.bottom;
 
   var x = d3.scale.ordinal()
-    .rangeRoundBands([0, width], 0.01);
+    .rangeRoundBands([0, width], 0.1);
 
   var xAxis = d3.svg.axis()
     .scale(x)
@@ -15,53 +17,57 @@
 
   // Create Linear Scale
   var y = d3.scale.linear()
-    .range([0, height]);
+    .range([height, 0]);
 
-  d3.tsv('data.tsv', type, function (error, data) {
-    x.domain(data.map(function(d) { return d.name; }));
+  var yAxis = d3.svg.axis()
+    .scale(y)
+    .orient('left')
+    .ticks(10, '%');
 
-    svg.append('g')
+  d3.tsv('letters.tsv', type, function (error, data) {
+    x.domain(data.map(function(d) { return d.letter; }));
+
+    chart.append('g')
       .attr('class', 'x axis')
-      .attr('transform', 'translate(' + margin.left + ',' + (svg.attr('height') - margin.bottom) + ')')
+      .attr('transform', 'translate(' + 0 + ',' + height + ')')
       .call(xAxis);
 
     y.domain([0, d3.max(data, function (d) {
-      return d.value;
+      return d.frequency;
     })]);
 
-    // Div Based Chart
-    var bars = svg.append('g').attr('transform', 'translate(' + margin.left + ',' + margin.top + ')')
-      .selectAll('g')
-      .data(data)
-      .enter().append('g')
-      .attr('transform', function (d) {
-        return 'translate(' + x(d.name) + ', 0)';
-      });
-
-    bars.append('rect')
+    chart.append('g')
+      .attr('class', 'y axis')
+      .call(yAxis)
+      .append('text')
       .attr({
-        y: function (d) { return height - y(d.value); },
+        transform: 'rotate(-90)',
+        y: 6,
+        dy: '0.71em'
+      })
+      .style('text-anchor', 'end')
+      .text('Frequency');
+
+    // Div Based Chart
+    var bars = chart
+      .selectAll('.bar')
+      .data(data)
+      .enter().append('rect')
+      .attr({
+        class: 'bar',
+        y: function (d) { return y(d.frequency); },
         height: function (d) {
-          return y(d.value);
+          return height - y(d.frequency);
         },
+        x: function (d) { return x(d.letter); },
         width: x.rangeBand()
       });
 
-    bars.append('text')
-      .attr({
-        x: x.rangeBand() / 2,
-        y: function (d) { return height - y(d.value); },
-        dy: '12px'
-      })
-      .text(function (d) {
-        return d.name;
-      });
-
-    bars.exit().remove();
+    //bars.exit().remove();
   });
 
   function type(d) {
-    d.value = +d.value; // coerce to number
+    d.frequency = +d.frequency; // coerce to number
     return d;
   }
 })();
